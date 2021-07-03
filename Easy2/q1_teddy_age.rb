617dbbd42c062b70acfa925f69a722c431b4fcb0
riazmohamed <railsdevriaz@gmail.com>
2021-07-03 22:26:32 +0100

    add teddy_age

---
+++ Easy2/q1_teddy_age.rb
@@ -0,0 +1,11 @@
+#Build a program that randomly generates and prints Teddy's age. To get the age, you should generate a random number between 20 and 200.
+# Example Output
+
+# Teddy is 69 years old!
+
+def teddy_age
+  age = rand(20..200)
+  puts "Teddy is #{age} years old!"
+end
+
+teddy_age
