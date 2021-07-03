a5c0fba1abf22b947f8582ffd4d2b290842a7f45
riazmohamed <railsdevriaz@gmail.com>
2021-07-03 21:06:49 +0100

    add reverse_wordsreverse_wordsreverse_words

---
+++ Easy1/q6_reverse_words.rb
@@ -0,0 +1,9 @@
+# Write a method that takes one argument, a string containing one or more words, and returns the given string with words that contain five or more characters reversed. Each string will consist of only letters and spaces. Spaces should be included only when more than one word is present.
+
+def reverse_words(str)
+  str.split.map { |e| e.length > 4 ? e.reverse : e }.join(" ")
+end
+
+p reverse_words('Professional')          # => lanoisseforP
+p reverse_words('Walk around the block') # => Walk dnuora the kcolb
+p reverse_words('Launch School')         # => hcnuaL loohcS
