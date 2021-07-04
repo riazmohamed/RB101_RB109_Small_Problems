3de289a4ce70053f6e30d28a3497c0c1fde8114e
riazmohamed <railsdevriaz@gmail.com>
2021-07-04 20:00:55 +0100

    q9_real_palindrome.rb

---
+++ Easy3/q9_real_palindrome.rb
@@ -0,0 +1,11 @@
+def real_palindrome?(str)
+  str_clean = str.scan(/\w/).join.downcase
+  str_clean == str_clean.reverse
+end
+
+p real_palindrome?('madam') == true
+p real_palindrome?('Madam') == true           # (case does not matter)
+p real_palindrome?("Madam, I'm Adam") == true # (only alphanumerics matter)
+p real_palindrome?('356653') == true
+p real_palindrome?('356a653') == true
+p real_palindrome?('123ab321') == false
