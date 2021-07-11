f8b972a030a8e3196b3ab8ada7a61a67aea07463
riazmohamed <railsdevriaz@gmail.com>
2021-07-11 11:42:07 +0100

    unadded files

--- Easy4/q1_short_long_short.rb
+++ Easy4/q1_short_long_short.rb
@@ -2,9 +2,9 @@ def short_long_short(str1, str2)
   hsh = [str1, str2].group_by {|e| e.length}
   min_arr = hsh[hsh.keys.min]
   max_arr = hsh[hsh.keys.max]
-  (min_arr + max_arr + min_arr).join
+  p(min_arr + max_arr + min_arr).join
 end
 
-p short_long_short('abc', 'defgh') == "abcdefghabc"
-p short_long_short('abcde', 'fgh') == "fghabcdefgh"
-p short_long_short('', 'xyz') == "xyz"
+short_long_short('abc', 'defgh') == "abcdefghabc"
+short_long_short('abcde', 'fgh') == "fghabcdefgh"
+short_long_short('', 'xyz') == "xyz"
